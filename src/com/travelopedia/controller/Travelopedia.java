a9076ebed56b2de821922f5c904ba20b3cf0eb23
chris lee <chrislee015@gmail.com>
2021-06-23 12:10:57 -0400

    Logo added

--- src/com/travelopedia/controller/Travelopedia.java
+++ src/com/travelopedia/controller/Travelopedia.java
@@ -1,10 +1,10 @@
 package com.travelopedia.controller;
 
-import com.travelopedia.view.MainMenu;
-import com.travelopedia.view.SearchMenu;
-import com.travelopedia.view.ViewMenu;
+import com.travelopedia.view.*;
+import com.travelopedia.view.TicketWindowFactory;
 
 import javax.swing.*;
+import java.awt.*;
 
 
 public class Travelopedia {
@@ -13,13 +13,14 @@ public class Travelopedia {
         // consoleStart();
         int newTicket;
         do {
-
+             TicketWindow ticketWindow = TicketWindowFactory.createTicketWindow();
              newTicket =  JOptionPane.showConfirmDialog(null,
                     "Would you like to schedule another flight?", "Travelopedia", JOptionPane.YES_NO_OPTION);
              if(newTicket != 0) JOptionPane.showMessageDialog(null,"Thank you for booking with us!"
-                                                        ,"Travelopedia"
-                                                        ,JOptionPane.PLAIN_MESSAGE
-                                                        ,new ImageIcon("src/images/niceday.jpg"));
+                     ,"Travelopedia"
+                     ,JOptionPane.PLAIN_MESSAGE
+                     ,new ImageIcon(new ImageIcon("src/images/travelopedia.png")
+                             .getImage().getScaledInstance(120,120, Image.SCALE_SMOOTH)));
         }while(newTicket == 0);
     }
 
