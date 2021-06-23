e025e92c9f968a6d45df058f8941d08c18ffb386
chathula777 <chathula@gmail.com>
2021-06-22 20:23:46 -0700

    updated LocationCodeTest with updated airport enum values

--- test/com/travelopedia/model/LocationCodeTest.java
+++ test/com/travelopedia/model/LocationCodeTest.java
@@ -7,19 +7,19 @@ import static org.junit.Assert.assertEquals;
 public class LocationCodeTest {
     @Test
     public void locationCodeReturnsCorrectLocations() {
-        assertEquals("San Diego, CA", Airport.SAN.getLocation());
-        assertEquals("Cleveland,OH", Airport.CLE.getLocation());
-        assertEquals("Seattle, WA", Airport.SEA.getLocation());
-        assertEquals("Newark, NJ", Airport.EWR.getLocation());
-        assertEquals("Denver, CO", Airport.DEN.getLocation());
+        assertEquals("San Diego, CA", Airport.SAN.getAirportCity());
+        assertEquals("Cleveland,OH", Airport.CLE.getAirportCity());
+        assertEquals("Seattle, WA", Airport.SEA.getAirportCity());
+        assertEquals("Newark, NJ", Airport.EWR.getAirportCity());
+        assertEquals("Denver, CO", Airport.DEN.getAirportCity());
     }
 
     @Test
     public void LocationCodeReturnsCorrectNames() {
-        assertEquals("Phoenix Sky Harbor International", Airport.PHX.getName());
-        assertEquals("Washington Dulles International", Airport.IAD.getName());
-        assertEquals("San Francisco International", Airport.SFO.getName());
-        assertEquals("Los Angeles International", Airport.LAX.getName());
-        assertEquals("Minneapolis St. Paul International", Airport.MSP.getName());
+        assertEquals("Phoenix Sky Harbor International", Airport.PHX.getAirportCode());
+        assertEquals("Washington Dulles International", Airport.IAD.getAirportCode());
+        assertEquals("San Francisco International", Airport.SFO.getAirportCode());
+        assertEquals("Los Angeles International", Airport.LAX.getAirportCode());
+        assertEquals("Minneapolis St. Paul International", Airport.MSP.getAirportCode());
     }
 }
