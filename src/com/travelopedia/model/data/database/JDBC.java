cf5446c136e7523b3bd3cdc4e483f944f788b4b2
chathula777 <chathula@gmail.com>
2021-06-23 20:50:07 -0700

    updated jdbc

--- src/com/travelopedia/model/data/database/JDBC.java
+++ src/com/travelopedia/model/data/database/JDBC.java
@@ -9,16 +9,15 @@ class JDBC {
     public static void main(String[] args) {
 
         try {
-
-
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/travelopediamysql", "root", "root");
-
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/travelopediamysql",
+                    "root", "root");
             Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("Select * from flights");
 
-            ResultSet resultSet = statement.executeQuery("Select * from Customer");
 
             while (resultSet.next()) {
-                System.out.println(resultSet.getString("firstName"));
+                System.out.println(resultSet.getString("airlineID"));
+
             }
         } catch (Exception e){
             e.printStackTrace();
